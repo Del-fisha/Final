60f05a80c04e07ce53ce35a0d43df8b686920c5b
Aleksey <del-fisha@yandex.ru>
2022-10-13 13:50:12 +0300

    Done

--- Program.cs
+++ Program.cs
@@ -1,24 +1,26 @@
-﻿string[] FirstArray = { "Hello", "Russia", "One", "Burn", "Tag", "Su", "Target" };
+﻿string[] FirstArray = new string [7] { "Hello", "Russia", "One", "Burn", "Tag", "Su", "Target" };
 string[] SecondArray = new string[FirstArray.Length];
 
-void SortArray(string array1, string array2)
+void SortArray(string[] array1, string[] array2)
 {
-    int Count = 0;
+    int help = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-        if(array1[i].Length <=3);
+        if(array1[i].Length <= 3)
         {
-        array2[count] = array1[i];
-        Count++;
+        array2[help] = array1[i];
+        help++;
         }
     }
 }
 
-void PrintArray(string array)
+void PrintArray(string[] array)
 {
-    for(int i=0; i< array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         Console.Write($"{array[i]} ");
     }
 }
 
+SortArray(FirstArray, SecondArray);
+PrintArray(SecondArray);
