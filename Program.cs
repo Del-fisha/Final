5fc9d9e13d285c39c13788087896762b3b92fc00
Aleksey <del-fisha@yandex.ru>
2022-10-13 13:37:30 +0300

    add_MethodSort

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿string[] FirstArray = {"Hello", "Russia", "One", "Burn", "Tag", "Su", "Target"};
+﻿string[] FirstArray = { "Hello", "Russia", "One", "Burn", "Tag", "Su", "Target" };
 string[] SecondArray = new string[FirstArray.Length];
+
+void SortArray(string array1, string array2)
+{
+    int Count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <=3);
+        {
+        array2[count] = array1[i];
+        Count++;
+        }
+    }
+}
